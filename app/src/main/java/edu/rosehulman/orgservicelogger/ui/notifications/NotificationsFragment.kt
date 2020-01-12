08d126fae47316b86a0446c9413ef4d68f40c6e1
Chris Gregory <gregorcj@rose-hulman.edu>
2020-01-12 18:35:21 -0500

    Add divider item decoration to notifications

--- app/src/main/java/edu/rosehulman/orgservicelogger/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/edu/rosehulman/orgservicelogger/ui/notifications/NotificationsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import edu.rosehulman.orgservicelogger.R
@@ -18,6 +19,7 @@ class NotificationsFragment : Fragment() {
         val recyclerView = inflater.inflate(R.layout.fragment_notifications, container, false) as RecyclerView
         recyclerView.adapter = NotificationsAdapter(context!!)
         recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         return recyclerView
     }
 }
