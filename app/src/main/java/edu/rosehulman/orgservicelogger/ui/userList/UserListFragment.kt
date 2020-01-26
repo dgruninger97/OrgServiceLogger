d0d13ad832db13727cd67a3237794127463a0dbd
Chris Gregory <gregorcj@rose-hulman.edu>
2020-01-26 00:34:30 -0500

    Add divider between each user name

--- app/src/main/java/edu/rosehulman/orgservicelogger/ui/userList/UserListFragment.kt
+++ app/src/main/java/edu/rosehulman/orgservicelogger/ui/userList/UserListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import edu.rosehulman.orgservicelogger.R
@@ -15,9 +16,16 @@ class UserListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val recyclerView = inflater.inflate(R.layout.fragment_user_list, container,false) as RecyclerView
+        val recyclerView =
+            inflater.inflate(R.layout.fragment_user_list, container, false) as RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = UserListAdapter(activity!!)
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
         return recyclerView
     }
 }
