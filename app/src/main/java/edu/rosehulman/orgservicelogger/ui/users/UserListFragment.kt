91a2693aa8e851d664615f5a34cdff7dc9e8d88e
Chris Gregory <gregorcj@rose-hulman.edu>
2020-01-25 23:46:32 -0500

    Cleanup UserListFragment

--- app/src/main/java/edu/rosehulman/orgservicelogger/ui/users/UserListFragment.kt
+++ app/src/main/java/edu/rosehulman/orgservicelogger/ui/users/UserListFragment.kt
@@ -1,14 +1,12 @@
 package edu.rosehulman.orgservicelogger.ui.users
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import edu.rosehulman.orgservicelogger.Constants
 import edu.rosehulman.orgservicelogger.R
 
 class UserListFragment : Fragment() {
@@ -17,12 +15,9 @@ class UserListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var userAdapter = UserListAdapter(activity!!)
-        val recycler_view = inflater.inflate(R.layout.fragment_user_list, container,false) as RecyclerView
-        recycler_view.layoutManager = LinearLayoutManager(context)
-        recycler_view.adapter = userAdapter
-        Log.d(Constants.TAG, "Adapter getting set")
-        return recycler_view
+        val recyclerView = inflater.inflate(R.layout.fragment_user_list, container,false) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = UserListAdapter(activity!!)
+        return recyclerView
     }
-
 }
