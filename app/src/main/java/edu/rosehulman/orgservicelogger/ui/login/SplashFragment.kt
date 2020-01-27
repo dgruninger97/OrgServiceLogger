5e8bc1a0b6f632dc788fc48cc96a96ef46af7233
Chris Gregory <gregorcj@rose-hulman.edu>
2020-01-27 13:53:14 -0500

    Reformat SplashFragment

--- app/src/main/java/edu/rosehulman/orgservicelogger/ui/login/SplashFragment.kt
+++ app/src/main/java/edu/rosehulman/orgservicelogger/ui/login/SplashFragment.kt
@@ -10,23 +10,17 @@ import android.view.ViewGroup
 import edu.rosehulman.orgservicelogger.R
 import kotlinx.android.synthetic.main.fragment_splash.view.*
 
-/**
- * A simple [Fragment] subclass.
- */
-class SplashFragment(val listener: OnLoginButtonPressedListener) : Fragment() {
+class SplashFragment(private val listener: OnLoginButtonPressedListener) : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_splash, container, false)
         view.fragment_splash_login_button.setOnClickListener {
             listener.onLoginButtonPressed()
         }
         return view
     }
-
-
 }
 
 interface OnLoginButtonPressedListener {
