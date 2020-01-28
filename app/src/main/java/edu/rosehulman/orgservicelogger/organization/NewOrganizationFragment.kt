4e87c0c995ffdd7a62aedc3be6e2591950db6115
dgruninger <grunindm@rose-hulman.edu>
2020-01-28 13:26:59 -0500

    Began working on the layout for adding a new organization

---
+++ app/src/main/java/edu/rosehulman/orgservicelogger/organization/NewOrganizationFragment.kt
@@ -0,0 +1,26 @@
+package edu.rosehulman.orgservicelogger.organization
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import edu.rosehulman.orgservicelogger.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class NewOrganizationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_organization, container, false)
+    }
+
+
+}
