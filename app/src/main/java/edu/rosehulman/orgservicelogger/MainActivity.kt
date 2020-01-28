82c1f2462345315bf5e4a68624c5f945e47c1514
dgruninger <grunindm@rose-hulman.edu>
2020-01-28 12:36:12 -0500

    Fixed toolbar menu

--- app/src/main/java/edu/rosehulman/orgservicelogger/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/orgservicelogger/MainActivity.kt
@@ -2,24 +2,33 @@ package edu.rosehulman.orgservicelogger
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 import edu.rosehulman.orgservicelogger.home.HomeFragment
 import edu.rosehulman.orgservicelogger.login.OnLoginButtonPressedListener
 import edu.rosehulman.orgservicelogger.login.SplashFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 private const val RC_SIGN_IN = 1
 
 class MainActivity : AppCompatActivity(), OnLoginButtonPressedListener {
     private lateinit var userId: String
     private lateinit var authStateListener: FirebaseAuth.AuthStateListener
-
+    private var auth:FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setSupportActionBar(toolbar)
         makeAuthStateListener()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 
     override fun onStart() {
@@ -35,6 +44,7 @@ class MainActivity : AppCompatActivity(), OnLoginButtonPressedListener {
     private fun makeAuthStateListener() {
         authStateListener = FirebaseAuth.AuthStateListener { auth: FirebaseAuth ->
             val user = auth.currentUser
+            this.auth = auth
             Log.d(Constants.TAG, "In the auth listener, user is $user")
 
             val fragment = if (user != null) {
@@ -60,5 +70,18 @@ class MainActivity : AppCompatActivity(), OnLoginButtonPressedListener {
             .setAvailableProviders(providers)
             .build()
         startActivityForResult(loginIntent, RC_SIGN_IN)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                auth?.signOut()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
