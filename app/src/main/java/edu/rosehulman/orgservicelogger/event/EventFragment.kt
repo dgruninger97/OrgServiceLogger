1a5adc69c5ff7abb326c968d8fe56c086ad168db
Chris Gregory <gregorcj@rose-hulman.edu>
2020-02-15 16:55:39 -0500

    Add attendees list to event page

--- app/src/main/java/edu/rosehulman/orgservicelogger/event/EventFragment.kt
+++ app/src/main/java/edu/rosehulman/orgservicelogger/event/EventFragment.kt
@@ -1,16 +1,21 @@
 package edu.rosehulman.orgservicelogger.event
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.FirebaseFirestore
+import edu.rosehulman.orgservicelogger.Constants
 import edu.rosehulman.orgservicelogger.R
-import edu.rosehulman.orgservicelogger.data.EventOccurrence
-import edu.rosehulman.orgservicelogger.data.EventSeries
-import edu.rosehulman.orgservicelogger.data.retrieveEvent
+import edu.rosehulman.orgservicelogger.data.*
 import edu.rosehulman.orgservicelogger.home.launchFragment
 import kotlinx.android.synthetic.main.fragment_event.view.*
+import kotlinx.android.synthetic.main.view_event_attendee.view.*
+import kotlinx.android.synthetic.main.view_event_attendee_signup.view.*
 import java.text.SimpleDateFormat
 
 class EventFragment(private val eventId: String) : Fragment() {
@@ -29,6 +34,33 @@ class EventFragment(private val eventId: String) : Fragment() {
             view.fragment_event_fab.setOnClickListener {
                 launchFragment(activity!!, EditEventFragment(event.id!!))
             }
+
+            val attendees = view.fragment_event_attendees
+            FirebaseFirestore.getInstance().collection("person")
+                .whereIn(FieldPath.documentId(), event.people.keys.toList())
+                .get()
+                .addOnSuccessListener { snapshot ->
+                    val people = snapshot.toObjects(Person::class.java)
+                    for (person in people) {
+                        val view = layoutInflater.inflate(R.layout.view_event_attendee, null, false)
+                        view.view_event_attendee_name.text = person.name
+                        if (!person.canDrive) {
+                            view.view_event_attendee_driving_icon.visibility = View.GONE
+                        }
+                        attendees.addView(view)
+                    }
+
+                    for (x in people.size until series.maxPeople) {
+                        val view =
+                            layoutInflater.inflate(R.layout.view_event_attendee_signup, null, false)
+                        view.view_event_attendee_signup_button.setOnClickListener {
+                            val personId = FirebaseAuth.getInstance().currentUser!!.uid
+                            Log.d(Constants.TAG, "Signing up $personId for event $eventId")
+                            TODO()
+                        }
+                        attendees.addView(view)
+                    }
+                }
         }
         return view
     }
